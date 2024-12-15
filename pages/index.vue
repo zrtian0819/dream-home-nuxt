5f7f3e8117be6bbba2cbca3e4d6e0fc21f1491ea
Joe_Zhang <cjt3399@gmail.com>
2024-12-15 20:23:00 +0800

    feat(all):建立初始內容

---
+++ pages/index.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="bg-white py-24">
+    <div class="flex flex-col items-center">
+      <div class="text-6xl font-semibold text-gray-800">這裡是首頁</div>
+    </div>
+    <div class="my-4 flex flex-col items-center space-x-4">
+      <nuxt-link to="/login" class="text-4xl text-gray-700">前往登入</nuxt-link>
+    </div>
+  </div>
+</template>
---
+++ pages/login.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="flex h-screen w-screen items-center justify-center">
+    <div class="flex flex-col items-center">
+      <div class="test text-6xl font-semibold text-gray-800">這裡是登入頁</div>
+    </div>
+  </div>
+</template>
